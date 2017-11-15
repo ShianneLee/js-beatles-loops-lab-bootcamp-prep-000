9d0ff4fc33bd6ba18ff9f7c06281c1338eaf2233
Shianne Lee <shiannelee@live.ca>
2017-11-15 02:14:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = {}
+  for(let i = 0; i < musicians.length; i++){
+    array[i] = "${musicians[i]} plays ${instruments[i]}"
+  }
+  return array
+}
+function johnLennonFacts(array){
+  
+}
